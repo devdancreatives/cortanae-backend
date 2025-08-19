eceed1beb7b4d738e79167ece6951325a37e810b
OnlyGod <onlygodovbije@gmail.com>
2025-08-20 00:06:05 +0100

    added allow orign

--- cortanae/settings.py
+++ cortanae/settings.py
@@ -183,3 +183,8 @@ CLOUDINARY = {
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 MEDIA_URL = "/media/"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000", 
+    "http://127.0.0.1:3000",
+]
