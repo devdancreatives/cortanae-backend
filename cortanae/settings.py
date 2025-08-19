a5fae912d534102ade1a2e3bc14b2988cbb1f3f3
OnlyGod <onlygodovbije@gmail.com>
2025-08-20 00:04:40 +0100

    added corsheaders

--- cortanae/settings.py
+++ cortanae/settings.py
@@ -63,6 +63,7 @@ LOCAL_APPS = [
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
