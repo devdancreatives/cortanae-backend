42184b9d03fbfeddfba0830b6c60f25305642206
OnlyGod <onlygodovbije@gmail.com>
2025-08-31 18:28:34 +0100

    updated cors

--- cortanae/settings.py
+++ cortanae/settings.py
@@ -212,6 +212,7 @@ MEDIA_URL = "/media/"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000", 
     "http://127.0.0.1:3000",
+    "https://cortanae.com",
     "https://cortanae-frontend.vercel.app"
     
 ]
