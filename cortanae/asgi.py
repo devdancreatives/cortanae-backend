51591efe30ce456295dfcad7b42138050195b7d7
OnlyGod <onlygodovbije@gmail.com>
2025-09-01 12:44:27 +0100

    debuging

--- cortanae/asgi.py
+++ cortanae/asgi.py
@@ -1,16 +1,19 @@
-"""
-ASGI config for myproject project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
-"""
-
 import os
-
-from django.core.wsgi import get_wsgi_application
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+import apps.chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cortanae.settings")
 
-application = get_wsgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(apps.chat.routing.websocket_urlpatterns)
+        )
+    ),
+})
--- cortanae/settings.py
+++ cortanae/settings.py
@@ -248,8 +248,15 @@ LOGGING = {
     },
 }
 
-ASGI_APPLICATION = "cortanae.routing.application"
+ASGI_APPLICATION = "cortanae.asgi.application"
 
-CHANNEL_LAYERS = {
-    "default": {"BACKEND": "channels.layers.InMemoryChannelLayer"},
-}
+
+if ENVIRONMENT == "local":
+    CHANNEL_LAYERS = {"default": {"BACKEND": "channels.layers.InMemoryChannelLayer"}}
+else:
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "channels_redis.core.RedisChannelLayer",
+            "CONFIG": {"hosts": [("127.0.0.1", 6379)]},
+        }
+    }
