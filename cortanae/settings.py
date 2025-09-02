625634d9f066bdcf069ba5d8f764842d31d5e18c
Oreoluwa Akinbo <oreoluwaakinbo.oa@gmail.com>
2025-09-02 09:54:52 +0100

    create notifications websocket

--- cortanae/settings.py
+++ cortanae/settings.py
@@ -198,9 +198,10 @@ if ENVIRONMENT == "local":
 else:
     # Gmail SMTP backend for production
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST = "smtp.gmail.com"
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
+    EMAIL_HOST = "smtp.hostinger.com"
+    EMAIL_PORT = 465
+    EMAIL_USE_TLS = False
+    EMAIL_USE_SSL = True
     EMAIL_HOST_USER = config("EMAIL_HOST_USER")
     DEFAULT_FROM_EMAIL = config("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
