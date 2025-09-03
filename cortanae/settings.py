cc677f78b9e1f8a1582d16b7e560a2700799d240
OnlyGod <onlygodovbije@gmail.com>
2025-09-03 20:21:34 +0100

    Fixup

--- cortanae/settings.py
+++ cortanae/settings.py
@@ -221,6 +221,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "https://cortanae.com",
+    "www.cortanae.com",
+    "https://www.cortanae.com",
     "https://cortanae-frontend.vercel.app",
 ]
 
