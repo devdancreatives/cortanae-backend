2543bb553ff1921fb7e571e9c9c56448b849ddb8
OnlyGod <onlygodovbije@gmail.com>
2025-09-04 23:00:34 +0100

    FIxup

--- apps/users/models.py
+++ apps/users/models.py
@@ -8,7 +8,7 @@ from django_countries.fields import CountryField
 class User(AbstractUser, BaseModelMixin):
     phone_number = models.CharField(max_length=20, unique=True, null=True)
     is_verified = models.BooleanField(default=False)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, blank=True, null=True)
     country = CountryField()
     verification_token = models.CharField(max_length=500, blank=True, null=True)
     is_deleted = models.BooleanField(default=False)
