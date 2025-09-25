69a05dabd528af0b1a7f018742afa8e69eb76c2f
OnlyGod <onlygodovbije@gmail.com>
2025-09-25 09:20:57 +0100

    added logs

--- apps/notifications/service/notification_service.py
+++ apps/notifications/service/notification_service.py
@@ -19,6 +19,8 @@ from cortanae.generic_utils.mail_send import mail_service
 
 User = get_user_model()
 
+import logging
+db_logger = logging.getLogger("db")
 
 def send_notification(user, content, title, type, mail_options=None):
     Notification.objects.create(
@@ -32,6 +34,7 @@ def send_notification(user, content, title, type, mail_options=None):
 def send_push_notification(
     title: str, body: str, single_user: bool, user: User | None
 ):
+    db_logger.info(f"[WS][PUSH] Sending push notification to {user}")
     push_service = FCMNotification(
         service_account_file=os.path.join(BASE_DIR, settings.SERVICE_FILE)
     )
@@ -50,25 +53,31 @@ def send_push_notification(
                     notification_title=title,
                     notification_body=body,
                 )
+                db_logger.info(f"[WS][PUSH] FCM response: {result}")
                 if result.get("name"):
                     continue
             except FCMServerError as e:
+                db_logger.error(f"[WS][PUSH] FCM server error for token {token}")
                 raise RuntimeError(
                     "An error occured that the FCM server could not process"
                 ) from e
             except AuthenticationError as e:
+                db_logger.warning(f"[WS][PUSH] Authentication error for token {token}")
                 raise PermissionError(
                     "User Firebase account cannot be authenticated at the present time"
                 ) from e
             except InvalidDataError as e:
+                db_logger.warning(f"[WS][PUSH] Invalid data for token {token}")
                 raise ValueError(
                     "Data passed to method is not of the right structure"
                 ) from e
             except FCMSenderIdMismatchError as e:
+                db_logger.warning(f"[WS][PUSH] Sender ID mismatch for token {token}")
                 raise PermissionError(
                     "Credentials of sender not the same as authenticated sender on FCM"
                 ) from e
             except FCMNotRegisteredError as e:
+                db_logger.warning(f"[WS][PUSH] Deleting invalid token {token}")
                 FCMDevice.objects.filter(token=token).delete()
                 raise ValueError(
                     "Token missing or invalid. Confirm if the device is registered"
